e190a5ea6b10ab88914ca8a74e6d181fcc63d319
Arthur Vincent Simon <developarvin@gmail.com>
2020-06-30 23:07:08 +0800

    Add messages

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,12 @@ import "./App.css";
 
 function App() {
   const [points, setPoints] = useState<number>(0);
+  const [messages, setMessages] = useState<string[]>([]);
+
   const increment = (points: number) => {
-    setPoints(points + 1);
+    const result = points + 1;
+    setPoints(result);
+    setMessages([`${points} was incremented to ${result}`, ...messages]);
   };
   const decrement = (points: number) => {
     let result = points - 1;
@@ -12,14 +16,22 @@ function App() {
       return;
     }
     setPoints(result);
+    setMessages([`${points} was decremented to ${result}`, ...messages]);
   };
 
   return (
     <div className="App">
       <h1>Trivia Points Tracker</h1>
-      <input value={points} />
-      <button onClick={() => increment(points)}>Up</button>
-      <button onClick={() => decrement(points)}>Down</button>
+      <div>
+        <input value={points} />
+        <button onClick={() => increment(points)}>Up</button>
+        <button onClick={() => decrement(points)}>Down</button>
+      </div>
+      <div>
+        {messages.map((message) => {
+          return <div>{message}</div>;
+        })}
+      </div>
     </div>
   );
 }
